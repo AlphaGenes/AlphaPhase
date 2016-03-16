86e2ff18d2ab975e86e113e4474bb7116d3c6ea3
Daniel Money <daniel.money@roslin.ed.ac.uk>
2016-03-16 09:55:51 +0000

    Compile.sh update.

--- Compile.sh
+++ Compile.sh
@@ -1,1 +1,1 @@
-ifort AlphaPhase.f90 -i-static -O3 -m64 -o AlphaPhase1.1.1
+ifort  -openmp -static-intel -fpp -openmp-link=static -O3 -m64 -o alphaphase Random.f90 DataSubset.f90 HaplotypeLibrary.f90 Phasing.f90 SurrogateDefinition.f90 AlphaPhase.f90
