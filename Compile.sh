3a52127fc186da5d52822c721aafae7abcc5bddf
ANTOLIN Roberto <rantolin@c161088.wlan.net.ed.ac.uk>
2016-04-19 13:31:50 +0100

    Update compilation script to support modules

--- Compile.sh
+++ Compile.sh
@@ -1,1 +1,2 @@
-ifort AlphaPhase.f90 -i-static -O3 -m64 -o AlphaPhase1.1.1.beta
+ifort -syntax-only AlphaPhase.f90 2> /dev/null
+ifort -static-intel -fpp -O3 -m64 -o AlphaPhase1.1 Random.f90 CoreDefinition.f90 DataSubset.f90 SurrogateDefinition.f90 HaplotypeLibrary.f90 Phasing.f90 NRMcode.f90 AlphaPhase.f90
