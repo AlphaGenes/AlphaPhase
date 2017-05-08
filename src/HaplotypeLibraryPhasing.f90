89463c16e6c14c98f973135ca2ce55a16815b7fa
MONEY Daniel <Daniel.Money@roslin.ed.ac.uk>
2017-05-08 12:59:33 +0100

    Same again

--- src/HaplotypeLibraryPhasing.f90
+++ src/HaplotypeLibraryPhasing.f90
@@ -24,6 +24,9 @@ contains
     minpresent = max(int(percminpresent * c%getNCoreSnp()),minoverlap)
     
     do i = 1, c % getNAnisG()
+      if (mod(i,2000) == 0) then
+	print *, "Done", i
+      end if
       !Paternal Haps
       hap = c % phase(i, 1)
       if (hap%numberNotMissing() >= minpresent) then
