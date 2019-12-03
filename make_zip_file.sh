cc977a0c3794db36b708a62ba59a0ec98c7b4612
WHALEN Andrew <awhalen@mvm-ri-m155130.roslin.ed.ac.uk>
2019-12-03 15:55:10 +0000

    Adding some auto build info

---
+++ make_zip_file.sh
@@ -0,0 +1,28 @@
+mkdir AlphaPhase
+
+# Assumes that the program and manual have both been built.
+
+# To build the program run:
+# NOTE: Binaries should be moved to the "binaries" folder and uploaded to bitbucket after builds.
+#cmake . ; make
+
+# to build the manual using Sphinx:
+#( cd alphaphase-docs ; make latexpdf )
+
+cp -r example AlphaPhase
+
+# Copy in the documentation.
+cp AlphaPhase-docs/build/latex/AlphaPhase.pdf AlphaPhase/AlphaPhase.pdf
+
+# Copy in the binaries
+cp binaries/* AlphaPhase
+
+# Create a version file
+
+version=`git describe --tags --abbrev=0`
+commit=`git rev-parse --short HEAD`
+
+echo Version: $version > AlphaPhase/version.txt
+echo Commit: $commit >> AlphaPhase/version.txt
+
+zip -r AlphaPhase.zip AlphaPhase
